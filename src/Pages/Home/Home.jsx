ddbbeb43c932858acdeb5ee54ac2ae82e71aa452
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2024-06-27 17:56:11 +0530

    importing images and creating herosection

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import HeroSection from '../../Components/HeroSection'
+import About from '../../Components/About'
+import Footer from '../../Components/Footer'
+import Qualities from '../../Components/Qualities'
+import Menu from '../../Components/Menu'
+import WhoAreWe from '../../Components/WhoAreWe'
+import Team from '../../Components/Team'
+import Reservation from '../../Components/Reservation'
+
+
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+    <HeroSection/>
+    <About/>
+    <Qualities/>
+    <Menu/>
+    <WhoAreWe/>
+    <Team/>
+    <Reservation/>
+    <Footer/>
+    </>
   )
 }
 
