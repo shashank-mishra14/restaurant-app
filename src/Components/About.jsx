857b9044a76f96d148baeec4e6985b58926f7b64
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2024-07-01 18:24:43 +0530

    pushing the right code

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,10 +1,40 @@
-import React from "react";
+import React from 'react'
 import {Link} from "react-router-dom"
 import { HiOutlineArrowNarrowRight } from "react-icons/hi";
 const About = () => {
   return (
-    <div>About</div>
+   
+    <section className="about" id="about">
+      <div className='container'>
+        <div className='banner'>
+          <div className='top'>
+            <h1 className='heading'>
+              About Us
+            </h1>
+            <p>The only thing we're serious about is food.</p>
+          </div>
+          <p className='mid'>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Provident possimus optio adipisci dolores impedit illum iusto
+              perferendis, laudantium quod accusamus consequuntur consectetur,
+              tempore nulla error iure reiciendis dolorem assumenda.
+              Necessitatibus fugit asperiores totam rem esse exercitationem
+              iusto ipsum qui dolore ex, accusantium repellat mollitia
+              repellendus.
+          </p>
+          {/* <Link to={"/"}>
+          Explore Menu{""}
+          <span>
+            <HiOutlineArrowNarrowRight/>
+          </span>
+          </Link> */}
+        </div>
+        <div className='banner'> 
+          <img src="about.png" alt="about" />
+        </div>
+      </div>
+    </section>
   )
 }
 
-export default About;
+export default About
