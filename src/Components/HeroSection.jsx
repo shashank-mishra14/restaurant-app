28512bbd3ccd50339384270b6bcb97fdd358b461
CharanKumarKharade <119148785+CharanKumarKharade@users.noreply.github.com>
2024-07-02 20:01:44 +0530

    Revert "added tailwind css1"

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -1,40 +1,37 @@
-import React from "react";
-import Navbar from "./Navbar";
+import React from 'react'
+import Navbar from './Navbar'
 const HeroSection = () => {
   return (
-    <section
-      class="flex flex-col min-h-screen justify-between items-center bg-cover bg-right bg-no-repeat py-16 px-4"
-      style="background-image: url('/restaurant-app/public/background.svg')"
-    >
-      <Navbar />
-      <div className="container">
-        <div className="banner">
-          <div className="largeBox">
-            <h1 className="title">Delicious</h1>
-          </div>
-          <div className="combined_boxes">
-            <div className="imageBox">
-              <img src="./hero1.png" alt="hero" />
+    <section className='heroSection' id="herosection">
+      <Navbar/>
+      <div className='container'>
+        <div className='banner'>
+          <div  className='largeBox'>
+            <h1 className='title'>Delicious</h1>
+          </div> 
+          <div className='combined_boxes'>
+            <div className='imageBox'>
+                <img src="./hero1.png" alt="hero" />
             </div>
-            <div className="textAndLogo">
-              <div className="textWithSvg">
-                <h1 className="title">Food</h1>
-                <h1 className="title dishes_title">Dishes</h1>
-                <img src="./threelines.svg" alt="" />
+              <div className='textAndLogo'>
+                <div className='textWithSvg'> 
+                  <h1 className='title'>Food</h1>
+                  <h1 className='title dishes_title'>Dishes</h1>
+                  <img src="./threelines.svg" alt="" />
+                </div>
+                <img className="logo" src="logo.svg" alt="" />
               </div>
-              <img className="logo" src="logo.svg" alt="" />
-            </div>
           </div>
         </div>
-        <div className="banner">
-          <div className="imageBox">
+        <div className='banner'>
+          <div className='imageBox'>
             <img src="hero2.png" alt="" />
           </div>
-          <h1 className="title dishes_title">Dishes</h1>
+          <h1 className='title dishes_title'>Dishes</h1>
         </div>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default HeroSection;
+export default HeroSection
