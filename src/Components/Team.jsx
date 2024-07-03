c31273906c4b5c6c5747cc805090523493195e98
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2024-07-03 17:55:15 +0530

    applied changes to the team section and who are we section

--- src/Components/Team.jsx
+++ src/Components/Team.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import data from "../restApi.json";
 
 const Team = () => {
   return (
-    <div>Team</div>
-  )
-}
-
-export default Team
+    <section className="team" id="team">
+      <div className="container">
+        <div className="heading_section">
+          <h1 className="heading">Our Team</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+            fugit dicta, ipsum impedit quam laboriosam quas doloremque quia
+            perferendis laborum.
+          </p>
+        </div>
+        <div className="team_container">
+          {
+            data.data[0].team.map((element) =>{
+              return (
+                <div className="card" key={element.id}>
+                  <img src={element.image} alt="team" />
+                  <h2>{element.name}</h2>
+                  <p>{element.designation}</p>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </section>
+    );
+  };
+  
+  export default Team;
