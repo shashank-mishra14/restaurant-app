a07f5188038937da1a14481285f22122888206ef
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2024-07-03 18:36:51 +0530

    create make reservation form and navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from "react";
+import data from "../restApi.json";
+import { Link } from "react-scroll";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = () => {
+  const [show, setShow] = useState(false);
   return (
-    <div>Navbar</div>
-  )
-}
+    <>
+      <nav>
+        <div className="logo">Goodness Bowl</div>
+        <div className={show ? "navLinks showmenu" : "navLinks"}>
+          <div className="links">
+          {
+            data.data[0].navbarLinks.map((element)=>(
+              <Link
+              to={element.link}
+              spy={true}
+              smooth={true}
+              duration={500}
+              key={element.id}
+            >
+              {element.title}
+            </Link>
+          ))}
+        </div>
+        <button className="menuBtn">OUR MENU</button>
+        </div>
+        <div className="hamburger" onClick={()=>setShow(!show)}>
+          <GiHamburgerMenu />
+        </div>
+      </nav>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/Components/Reservation.jsx
+++ src/Components/Reservation.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import React from "react";
+import { HiOutlineArrowNarrowRight } from "react-icons/hi";
+// import axios from "axios";
+// import { useState } from "react";
+// import toast from "react-hot-toast";
+// import { useNavigate } from "react-router-dom";
 
 const Reservation = () => {
   return (
-    <div>Reservation</div>
+    <section className="reservation" id="reservation">
+      <div className="container">
+        <div className="banner">
+          <img src="/reservation.png" alt="res" />
+
+        </div>
+        <div className="banner">
+        <div className="reservation_form_box">
+          <h1>MAKE A RESERVATION</h1>
+          <p>For Further Questions, Please Call</p>
+          <form>
+            <div>
+              <input type="text"
+              placeholder="First Name" />
+              <input type="text" placeholder="Last Name"/>
+            </div>
+            <div>
+              <input type="date" placeholder="Date"/>
+              <input type="time" placeholder="Time"/>
+            </div>
+            <div>
+              <input type="email" placeholder="Email"/>  
+              <input type="number" placeholder="Phone"/>
+
+            </div>
+            <button type="submit">
+              Reserve Now{""}
+
+              <span>
+                <HiOutlineArrowNarrowRight />
+              </span>
+            </button>
+          </form>
+        </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
